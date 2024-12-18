cc7addb1196c7b8ebbeb18db392dac687c2d92d4
Arthur <odthientho@gmail.com>
2024-12-18 11:48:14 -0500

    initialize Gin Job Tracker project

---
+++ src/main/java/com/gin/job_tracker/GinJobTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gin.job_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GinJobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GinJobTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gin/job_tracker/GinJobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gin.job_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinJobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
